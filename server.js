ee93b554a847ccc723f171c7464ff2f504f94003
manikantkg <manikantacoder007@gmail.com>
2023-06-21 15:15:27 +0530

    new api added

--- server.js
+++ server.js
@@ -8,11 +8,18 @@ app = express();
 app.get('/', (req, res) => {
     console.log("hello")
     res.send("Hello World!!!")
+});
+
+
+app.get('/hai', (req, res) => {
+    console.log("hai")
+    res.send("Hai World!!!")
 })
 
 
 
 
+
 app.listen(8080, function(){
-    console.log("Server is running at 9099!!!!")
+    console.log("Server is running at 8080!!!!")
 } )
